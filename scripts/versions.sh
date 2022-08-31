dc76a970e37f057dbbecb2b688f05dad9440cffc
Volkan Kukrer <volkan.kukrer@gmail.com>
2022-08-31 17:13:28 +0300

    change avalanche to savannah

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -3,4 +3,4 @@
 # Set up the versions to be used
 coreth_version=${CORETH_VERSION:-'v0.8.16'}
 # Don't export them as they're used in the context of other calls
-savannah_version=${AVALANCHE_VERSION:-'v1.7.17'}
+savannahnode_version=${SAVANNAHNODE_VERSION:-'v1.7.17'}
